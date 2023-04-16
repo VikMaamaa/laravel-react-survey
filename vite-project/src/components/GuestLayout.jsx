e03c0bcb6f633a9c78cfb28a12bc10d7bb233de6
vikmaamaa <maamaavictor@yahoo.com>
2023-04-16 23:12:22 +0100

    feat: protect routes

--- vite-project/src/components/GuestLayout.jsx
+++ vite-project/src/components/GuestLayout.jsx
@@ -1,6 +1,13 @@
-import { Outlet } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
+import { userStateContext } from "../contexts/ContextProvider";
 
 export default function GuestLayout() {
+  const {currentUser, userToken} = userStateContext()
+
+  if(userToken){
+    return <Navigate to={'/'} />
+  }
+
 
   return (
     <>
